697ee9289b5c30687ac840f543feb671e10c4747
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-10-22 16:03:14 -0700

    added request mapping

--- src/main/java/com/example/chatroom/ChatroomApplication.java
+++ src/main/java/com/example/chatroom/ChatroomApplication.java
@@ -2,7 +2,9 @@ package com.example.chatroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/")
 @SpringBootApplication
 public class ChatroomApplication {
 
